400e0b481ee417810c306fc0b2d4e4c1a34f05ef
aneeszaid <34118764+aneeszaid@users.noreply.github.com>
2026-05-27 17:03:07 +0530

    Decrease timeout for requests to 20 seconds
    Reduced timeout for HTTP requests from 40 to 20 seconds.

--- redirect-check-1.py
+++ redirect-check-1.py
@@ -29,7 +29,7 @@ for original_url in urls:
                 url,
                 headers=headers,
                 allow_redirects=True,
-                timeout=40,
+                timeout=20,
                 verify=False
             )
         except:
@@ -38,7 +38,7 @@ for original_url in urls:
                 url,
                 headers=headers,
                 allow_redirects=True,
-                timeout=40,
+                timeout=20,
                 verify=False
             )
 
